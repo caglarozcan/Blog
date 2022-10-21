8b7c89901abfe33db0c01cb6e0dc17798941ca81
Çağlar ÖZCAN <caglar.ozcan@izu.edu.tr>
2022-10-21 10:55:30 +0300

    Dosya yükleme kısmında geliştirmeler yapıldı.

--- src/infrastructure/Blog.Infrastructure/Services/FileIOService.cs
+++ src/infrastructure/Blog.Infrastructure/Services/FileIOService.cs
@@ -2,6 +2,7 @@
 using Blog.Application.Services;
 using Blog.Application.UnitOfWork;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
 
 namespace Blog.Infrastructure.Services
 {
@@ -29,29 +30,47 @@ namespace Blog.Infrastructure.Services
 
 		public async Task<Response> Create(IFormFile file)
 		{
-			var mimeTypeWhiteList = await _unitOfWork.MediaTypeReadRepository.GetMediaTypeWhiteListAsync();
+			//TODO : Yüklenen dosya resimse appsettingsten okunan değerler ile resimleri boyutlandır ve farklı klasörler içerisine at.
+			//TODO : Dosya yükleme dizininde sadece okuma yazma izni olacak. Çalıştırma izni olmayacak.
 
-			if(mimeTypeWhiteList.Any(m => m.MimeType.Equals(file.ContentType)))
+			if (file.Length > 0)
 			{
-				var fileInfo = mimeTypeWhiteList.FirstOrDefault(m => m.MimeType.Equals(file.ContentType));
-				string newFileName = Guid.NewGuid().ToString() + fileInfo.FileExtension;
-				await _unitOfWork.MediaWriteRepository.InsertAsync(new Domain.Entities.Media()
+				var mimeTypeWhiteList = await _unitOfWork.MediaTypeReadRepository.GetMediaTypeWhiteListAsync();
+
+				if (mimeTypeWhiteList.Any(m => m.MimeType.Equals(file.ContentType)))
 				{
-					CreatedDate = DateTime.Now,
-					Description = "Blog dosyası.",
-					MediaTypeId = fileInfo.Id,
-					Name = newFileName,
-					OriginalName = file.Name,
-					UserId = _authUserInfoService.Id
-				});
+					var fileInfo = mimeTypeWhiteList.FirstOrDefault(m => m.MimeType.Equals(file.ContentType));
+					string newFileName = String.Concat(Guid.NewGuid().ToString(), fileInfo.FileExtension);
 
-				int status = await _unitOfWork.SaveAsync();
+					var uploadPath = Path.Combine(@"C:\Users\caglar.ozcan\source\Workspaces\Blog\src\presentation\Blog.Web\Uploads\", fileInfo.UploadDir, @"\", newFileName);
 
-				return new Response("Dosya başarıyla yüklendi.", true);
+					using (var stream = System.IO.File.Create(uploadPath))
+					{
+						await file.CopyToAsync(stream);
+					}
+
+					await _unitOfWork.MediaWriteRepository.InsertAsync(new Domain.Entities.Media()
+					{
+						CreatedDate = DateTime.Now,
+						Description = "Blog dosyası.",
+						MediaTypeId = fileInfo.Id,
+						Name = newFileName,
+						OriginalName = file.Name,
+						UserId = _authUserInfoService.Id
+					});
+
+					await _unitOfWork.SaveAsync();
+
+					return new Response("Dosya başarıyla yüklendi.", true);
+				}
+				else
+				{
+					return new Response(String.Concat(file.ContentType, " bu türde dosya yüklenemez."), false);
+				}
 			}
 			else
 			{
-				return new Response(String.Concat(file.ContentType, " bu türde dosya yüklenemez."), false);
+				return new Response("Dosya seçmelisiniz.", false);
 			}
 		}
 
