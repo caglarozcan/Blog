ed39b760251cb87d4258770a9b4f48cc30f8bed1
Çağlar ÖZCAN <caglar.ozcan@izu.edu.tr>
2022-12-15 16:13:12 +0300

    Medya işlemlerinde yüklenen resim dosyası için boyutlandırma eklendi.

--- src/infrastructure/Blog.Infrastructure/Services/FileIOService.cs
+++ src/infrastructure/Blog.Infrastructure/Services/FileIOService.cs
@@ -1,9 +1,12 @@
 ﻿using Blog.Application.Dto.SettingDto.BlogOptions;
+using Blog.Application.Enums;
 using Blog.Application.Response;
 using Blog.Application.Services;
 using Blog.Application.UnitOfWork;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Options;
+using System.Drawing;
+using System.Runtime.Versioning;
 
 namespace Blog.Infrastructure.Services;
 
@@ -14,8 +17,8 @@ public class FileIOService : IFileIOService
 	private readonly FileUploadOptions _fileUploadOptions;
 
 	public FileIOService(
-		IUnitOfWork unitOfWork, 
-		IAuthUserInfoService authUserInfoService, 
+		IUnitOfWork unitOfWork,
+		IAuthUserInfoService authUserInfoService,
 		IOptions<FileUploadOptions> fileUploadOptions)
 	{
 		this._unitOfWork = unitOfWork;
@@ -34,11 +37,9 @@ public class FileIOService : IFileIOService
 		throw new NotImplementedException();
 	}
 
+	[SupportedOSPlatform("windows")]
 	public async Task<Response> Create(IFormFile file)
 	{
-		//TODO : Yüklenen dosya resimse appsettingsten okunan değerler ile resimleri boyutlandır ve farklı klasörler içerisine at.
-		//TODO : Dosya yükleme dizininde sadece okuma yazma izni olacak. Çalıştırma izni olmayacak.
-
 		if (file.Length > 0)
 		{
 			var mimeTypeWhiteList = await _unitOfWork.MediaTypeReadRepository.GetMediaTypeWhiteListAsync();
@@ -48,10 +49,10 @@ public class FileIOService : IFileIOService
 				var fileInfo = mimeTypeWhiteList.FirstOrDefault(m => m.MimeType.Equals(file.ContentType));
 				string newFileName = String.Concat(Guid.NewGuid().ToString(), fileInfo.FileExtension);
 
-				string uploadPath = @"C:\Users\caglar.ozcan\source\Workspaces\Blog\src\presentation\Blog.Web\wwwroot\"+ _fileUploadOptions.UploadPath + @"\" + fileInfo.UploadDir.Replace("/", "\\") + @"\original\" + newFileName;
-				//string uploadPath = @"D:\Projects\Visualstudio\Blog\src\presentation\Blog.Web\wwwroot\Uploads\" + fileInfo.UploadDir.Replace("/", "\\") + @"\" + newFileName;
+				string uploadPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", _fileUploadOptions.UploadPath, fileInfo.UploadDir.Replace("/", "\\")));
+				string fullPath = Path.Combine(uploadPath, "original", newFileName);
 
-				using (var stream = File.Create(uploadPath))
+				using (var stream = File.Create(fullPath))
 				{
 					await file.CopyToAsync(stream);
 				}
@@ -67,6 +68,13 @@ public class FileIOService : IFileIOService
 				});
 
 				await _unitOfWork.SaveAsync();
+
+				if (file.ContentType.StartsWith("image"))
+				{
+					resizeImageThumbnail(file.OpenReadStream(), uploadPath, newFileName, ImageResizeType.ResizeWithoutRatio);
+					resizeImageMedium(file.OpenReadStream(), uploadPath, newFileName, ImageResizeType.ResizeWithoutRatio);
+					resizeImageLarge(file.OpenReadStream(), uploadPath, newFileName, ImageResizeType.ResizeWithoutRatio);
+				}
 
 				return new Response("Dosya başarıyla yüklendi.", true);
 			}
@@ -91,18 +99,75 @@ public class FileIOService : IFileIOService
 		throw new NotImplementedException();
 	}
 
-	private async Task resizeImageThumbnail(Stream image)
+	[SupportedOSPlatform("windows")]
+	private void resizeImageThumbnail(Stream image, string uploadPath, string fileName, ImageResizeType imageResizeType)
 	{
-
+		if (imageResizeType == ImageResizeType.ResizeWithoutRatio)
+			imageResize(image, _fileUploadOptions.ImageSmallWidth, _fileUploadOptions.ImageSmallHeight).Save(Path.Combine(uploadPath, "thumbnail", fileName));
+		else
+			imageResizeRatio(image, _fileUploadOptions.ImageSmallWidth, _fileUploadOptions.ImageSmallHeight).Save(Path.Combine(uploadPath, "thumbnail", fileName));
 	}
 
-	private async Task resizeImageMedium(Stream image)
+	[SupportedOSPlatform("windows")]
+	private void resizeImageMedium(Stream image, string uploadPath, string fileName, ImageResizeType imageResizeType)
 	{
-
+		if (imageResizeType == ImageResizeType.ResizeWithoutRatio)
+			imageResize(image, _fileUploadOptions.ImageSmallWidth, _fileUploadOptions.ImageSmallHeight).Save(Path.Combine(uploadPath, "middle", fileName));
+		else
+			imageResizeRatio(image, _fileUploadOptions.ImageSmallWidth, _fileUploadOptions.ImageSmallHeight).Save(Path.Combine(uploadPath, "middle", fileName));
 	}
 
-	private async Task resizeImageLarge(Stream image)
+	[SupportedOSPlatform("windows")]
+	private void resizeImageLarge(Stream image, string uploadPath, string fileName, ImageResizeType imageResizeType)
 	{
+		if (imageResizeType == ImageResizeType.ResizeWithoutRatio)
+			imageResize(image, _fileUploadOptions.ImageSmallWidth, _fileUploadOptions.ImageSmallHeight).Save(Path.Combine(uploadPath, "large", fileName));
+		else
+			imageResizeRatio(image, _fileUploadOptions.ImageSmallWidth, _fileUploadOptions.ImageSmallHeight).Save(Path.Combine(uploadPath, "large", fileName));
+	}
 
+	[SupportedOSPlatform("windows")]
+	private Bitmap imageResize(Stream stream, int width, int height)
+	{
+		Image image = Image.FromStream(stream);
+		Bitmap bitmap = new Bitmap(Image.FromStream(stream), width, height);
+		using (Graphics graphics = Graphics.FromImage((Image)bitmap))
+		{
+			graphics.DrawImage(image, 0, 0, width, height);
+			return bitmap;
+		}
+	}
+
+	[SupportedOSPlatform("windows")]
+	private Bitmap imageResizeRatio(Stream stream, int width, int height)
+	{
+		Image image = Image.FromStream(stream);
+		Size size = new Size(width, height);
+
+		int imageWidth = image.Width;
+		int imageHeight = image.Height;
+
+		double ratio = 0;
+		double ratioWidth = (double)size.Width / (double)imageWidth;
+		double ratioHeight = (double)size.Height / (double)imageHeight;
+
+		if (ratioWidth < ratioHeight)
+		{
+			ratio = ratioWidth;
+		}
+		else
+		{
+			ratio = ratioHeight;
+		}
+
+		Size newSize = new Size((int)(imageWidth * ratio), (int)(imageHeight * ratio));
+
+		Bitmap bitmap = new Bitmap(newSize.Width, newSize.Height);
+		using (Graphics graphics = Graphics.FromImage((Image)bitmap))
+		{
+			graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+			graphics.DrawImage(image, 0, 0, newSize.Width, newSize.Height);
+			return bitmap;
+		}
 	}
 }
