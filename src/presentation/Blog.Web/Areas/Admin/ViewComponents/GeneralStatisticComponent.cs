9fe2cc8f920910314943afb1fed2f2c032e6d6cf
Çağlar ÖZCAN <caglar.ozcan@izu.edu.tr>
2022-12-23 13:04:13 +0300

    Dashboard için genel sayıları gösteren ViewComponent hazırlandı.

---
+++ src/presentation/Blog.Web/Areas/Admin/ViewComponents/GeneralStatisticComponent.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Admin.ViewComponents;
+
+public class GeneralStatisticComponent : ViewComponent
+{
+	private readonly IDashboardService _dashboardService;
+
+	public GeneralStatisticComponent(IDashboardService dashboardService)
+	{
+		_dashboardService = dashboardService;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		return View(await _dashboardService.GetGeneralStatisticAsync());
+	}
+}
