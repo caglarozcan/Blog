30edcbda0eb733a2e3b4832879cbb5b67c0e6996
caglar ozcan <cglrozcan@gmail.com>
2023-03-25 01:16:44 +0300

    Dark tema için css dosyaları düzenlendi.

---
+++ src/presentation/Blog.Web/Areas/Admin/ViewComponents/LastCommentsComponent.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Web.Areas.Admin.ViewComponents;
+
+public class LastCommentsComponent : ViewComponent
+{
+	private readonly IDashboardService _dashboardService;
+
+	public LastCommentsComponent(IDashboardService dashboardService)
+	{
+		_dashboardService = dashboardService;
+	}
+
+	public async Task<IViewComponentResult> InvokeAsync()
+	{
+		return View();
+	}
+}
