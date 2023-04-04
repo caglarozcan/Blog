20741923e285562b24c9f987c03eb68886e6d68a
Çağlar ÖZCAN <caglar.ozcan@izu.edu.tr>
2023-04-04 09:06:48 +0300

    Select componenti değiştirildi. Yeni bir yöntem ile kodlanacak.

--- src/presentation/Blog.Web/wwwroot/script/third-party/select.js
+++ src/presentation/Blog.Web/wwwroot/script/third-party/select.js
@@ -1,94 +1,116 @@
-﻿(function (root, factory) {
-	var plugin = "ChSelect";
+﻿function attr(element, key) {
+	if (element[key] != undefined) return element[key];
 
-	if (typeof exports === "object") {
-		module.exports = factory(plugin);
-	} else if (typeof define === "function" && define.amd) {
-		define([], factory(plugin));
-	} else {
-		root[plugin] = factory(plugin);
-	}
-})(typeof global !== "undefined" ? global : this.window || this.global, function (plugin) {
-	"use strict";
+	return element.getAttribute(key);
+}
 
-	var find = function (selector, element = document.documentElement) {
-		return Element.prototype.querySelector.call(element, selector);
-	}
+function data(element, key) {
+	return element.getAttribute('data-' + key);
+}
 
-	var findAll = function (selector, element = document.documentElement) {
-		return [].concat(
-			...Element.prototype.querySelectorAll.call(element, selector)
-		);
-	}
+function hasClass(element, className) {
+	if (element)
+		return element.classList.contains(className);
+	else
+		return false;
+}
 
-	var each = function (arr, fn, scope) {
-		var n;
-		if (isObject(arr)) {
-			for (n in arr) {
-				if (Object.prototype.hasOwnProperty.call(arr, n)) {
-					fn.call(scope, arr[n], n);
-				}
+function addClass(element, className) {
+	if (element) return element.classList.add(className);
+}
+
+function removeClass(element, className) {
+	if (element) return element.classList.remove(className);
+}
+
+var defaultOptions = {
+	data: null,
+	searchable: false
+}
+
+export default function ChSelect(element, options) {
+	this.element = element;
+	this.config = Object.assign({}, defaultOptions, options || {});
+	this.data = this.config.data;
+	this.selectedOptions = [];
+
+	this.placeholder = attr(this.element, 'placeholder') || this.config.placeholder || 'Bir değer seçiniz';
+	this.searchText = attr(this.element, 'searchtext') || this.config.searchText || 'Ara..';
+
+	this.dropdown = null;
+	this.multiple = attr(this.element, 'multiple');
+	this.disabled = attr(this.element, 'disabled');
+
+	this.create();
+};
+
+ChSelect.prototype.create = function () {
+	this.element.style.opacity = "0";
+	this.element.style.padding = "0";
+	this.element.style.width = "0";
+	this.element.style.height = "0";
+
+	if (this.data)
+		this.data = this.processData(this.data);
+	else
+		this.data = this.extractData();
+};
+
+ChSelect.prototype.processData = function (data) {
+	var options = [];
+
+	data.forEach(item => {
+		options.push({
+			data: item,
+			attributes: {
+				selected: !!item.selected,
+				disabled: !!item.disabled,
+				optgroup: item.value == 'optgroup'
 			}
-		} else {
-			for (n = 0; n < arr.length; n++) {
-				fn.call(scope, arr[n], n);
-			}
-		}
-	};
-
-	var ChSelect = function (selector, options) {
-		this.rootElement = find(selector);
-		this.options = options;
-		this.init();
-	}
-
-	var dchSelectProto = ChSelect.prototype;
-
-	dchSelectProto.init = function () {
-		var select = this;
-
-		var selectedTextElement = find('.selected-text', select.rootElement);
-
-		var selectContent = find('.select-content', select.rootElement);
-
-		var name = select.rootElement.dataset.name;
-
-		var placeHolder = select.rootElement.dataset.placeholder;
-		selectedTextElement.innerHTML = placeHolder;
-
-		var selectDropdown = find('.option-box', select.rootElement);
-
-
-		var inputHidden = document.createElement('input');
-		inputHidden.type = 'hidden';
-		inputHidden.name = name;
-		select.rootElement.appendChild(inputHidden);
-
-
-		var selectOptionContainer = find('ul', selectDropdown);
-
-		var options = findAll('li', selectOptionContainer);
-		options.forEach(function (item) {
-			if (item.classList.contains('selected')) {
-				inputHidden.value = item.dataset.value;
-			}
-
-			item.addEventListener('click', function (e) {
-				var optionValue = item.dataset.value;
-				inputHidden.value = optionValue;
-			});
 		});
+	});
 
-		selectContent.addEventListener('click', function (e) {
-			if (selectDropdown.classList.contains('hide')) {
-				selectDropdown.classList.remove('hide');
-				selectDropdown.classList.add('show');
-			} else {
-				selectDropdown.classList.remove('show');
-				selectDropdown.classList.add('hide');
-			}
-		}, true);
-	}
+	this.options = options;
+};
 
-	return ChSelect;
-});
+ChSelect.prototype.extractData = function () {
+	var options = this.element.querySelectorAll('option,optgroup');
+	var data = [];
+	var allOptions = [];
+	var selectedOptions = [];
+
+	options.forEach(item => {
+		if (item.tagName == 'OPTGROUP') {
+			var itemData = {
+				text: item.label,
+				value: 'optgroup'
+			};
+		} else {
+			var text = item.innerText;
+			if (item.dataset.display != undefined) text = dataset.display;
+
+			var itemData = {
+				text: text,
+				value: item.value,
+				selected: item.getAttribute('selected') != null,
+				disabled: item.getAttribute('disabled') != null
+			};
+		}
+
+		var attributes = {
+			selected: item.getAttribute('selected') != null,
+			disabled: item.getAttribute('disabled') != null,
+			optgroup: item.tagName == 'OPTGROUP'
+		};
+
+		data.push(itemData);
+		allOptions.push({ data: itemData, attributes: attributes });
+	});
+
+	this.data = data;
+	this.options = allOptions;
+	this.options.forEach(item => {
+		if (item.attributes.selected) selectedOptions.push(item);
+	});
+	this.selectedOptions = selectedOptions;
+};
