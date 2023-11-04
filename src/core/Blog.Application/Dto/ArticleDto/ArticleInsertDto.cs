83a24bcbecb1152b204bd1ea51f8f0d3284411ff
caglar ozcan <caglarozcan@hotmail.com.tr>
2023-11-04 22:56:25 +0300

    Makale ekleme ekranı için validation ve ajax post işlemi eklendi.

--- src/core/Blog.Application/Dto/ArticleDto/ArticleInsertDto.cs
+++ src/core/Blog.Application/Dto/ArticleDto/ArticleInsertDto.cs
@@ -15,8 +15,21 @@ namespace Blog.Application.Dto.ArticleDto
 		[Required(ErrorMessage = "Makale içeriği zorunludur.")]
 		public required string Content { get; set; }
 
+		[DataType(DataType.MultilineText)]
+		[DisplayName("Makale Açıklaması")]
+		public string? Description { get; set; } = default;
+
 		[DataType(DataType.Text)]
 		[DisplayName("Makale URL")]
 		public string? Slug { get; set; } = default;
+
+		[DataType(DataType.Text)]
+		[DisplayName("Makale kategorileri")]
+		[Required(ErrorMessage = "En az bir tane kategori seçmelisiniz.")]
+		public required List<Guid> Categories { get; set; }
+
+		[DataType(DataType.Text)]
+		[DisplayName("Kayıt Türü")]
+		public int SaveType { get; set; }
 	}
 }
--- src/presentation/Blog.Web/Areas/Admin/Controllers/ArticleController.cs
+++ src/presentation/Blog.Web/Areas/Admin/Controllers/ArticleController.cs
@@ -1,12 +1,39 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Blog.Application.Dto.ArticleDto;
+using Blog.Application.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Web.Areas.Admin.Controllers;
 
 [Area("Admin")]
 [Authorize(Roles = "Administrator, Editör")]
-public class ArticleController : Controller
+public class ArticleController : BaseController
 {
+	private readonly IArticleService _articleService;
+
+	public ArticleController(IArticleService articleService)
+	{
+		_articleService = articleService;
+	}
+
+	#region Functions
+	#region Create
+	[HttpPost]
+	public async ValueTask<IActionResult> Insert(ArticleInsertDto data)
+	{
+		var result = await _articleService.InsertAsync(data, ModelState);
+
+		if (true.Equals(result.Success))
+			return Ok(result);
+
+		return new ObjectResult(result.Value != null ? result.Value : result)
+		{
+			StatusCode = 400
+		};
+	}
+	#endregion
+
+	#region Read
 	public IActionResult Index()
 	{
 		return View();
@@ -21,4 +48,14 @@ public class ArticleController : Controller
 	{
 		return View();
 	}
+	#endregion
+
+	#region Update
+
+	#endregion
+
+	#region Delete
+
+	#endregion
+	#endregion
 }
